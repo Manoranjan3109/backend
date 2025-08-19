e5c4c8e4517b2ecfd463283e17c74802a35be7c9
Manoranjan3109 <manoranjan31122004.gmail.com>
2025-08-19 10:49:09 +0530

    updated

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -16,6 +16,7 @@ const protect = async (req, res, next) => {
     return res.status(401).json({ message: "Invalid token" });
   }
 };
+// only admin
 
 const adminOnly = (req, res, next) => {
   if (req.user?.role === "admin") return next();
